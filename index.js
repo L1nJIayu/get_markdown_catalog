50ccb453f4842ab4e1fda3ec7f60512677c38fb5
L1nJIayu <562316920@qq.com>
2024-01-08 18:27:21 +0800

    移除例子内容

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-
-
-
 require('./utils').getCatalogFile({
-  targetPath: 'E:\\BaiduSyncdisk\\学习笔记',
-  extname: ['.md', '.txt'],
-  outputFileName: '目录.md',
-  showExtname: true,
-  ignoreDirectoryPattern: /\.assets$/,
-  ignoreFilePattern: /^README.md$/
+  targetPath: './'
 })
